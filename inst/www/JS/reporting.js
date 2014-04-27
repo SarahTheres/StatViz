3f078b5643b385c0bf523905d0ad96d5ea892cce
SarahTheres <sarah-voelkel@gmx.de>
2014-04-27 14:36:28 +0200

    Update reporting.js
    change reporting dialog box

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -26,42 +26,23 @@ var canvas = d3.select("#plotCanvas");
             .attr("x", centerX - dialogBoxWidth/2)
             .attr("y", centerY - dialogBoxHeight/2)
             .attr("width", dialogBoxWidth)
-            .attr("height", dialogBoxHeight/3)
+            .attr("height", dialogBoxHeight)
             .attr("rx", "5px")
             .attr("ry", "5px")
             .attr("fill", "white")
             .attr("stroke", "grey")
             .attr("filter", "url(#shadow)")
-            .attr("id", "regression")
+            .attr("id", "reporting")
             .attr("class", "dialogBox");
     
-    var LEFT = (width - canvasWidth - sideBarWidth) + centerX - dialogBoxWidth/2;
-    var TOP = centerY - dialogBoxHeight/2;
-    
-    var divElement = d3.select("body").append("div").attr("style", "position: absolute; left: " + LEFT + "px; top: " + TOP + "px; height: " + dialogBoxHeight + "px; width: " + dialogBoxWidth + "px; text-align: center;").attr("class", "dialogBox");
-
-    var normality = d3.select("#normality.crosses");
-    var inText = d3.select("#normality.crosses").attr("display") == "inline" ? "POPULATION MEDIAN = " : "POPULATION MEAN = ";
-    
-    divElement.append("label")
-                .attr("align", "center")
-                .attr("vertical-align", "middle")
-                .attr("style", "font:1.2em \"Lucida Sans Unicode\", \"Lucida Grande\", sans-serif; color: black; padding-top: 10px;")
-                .text(inText);
-    divElement.append("input")
-                .attr("type", "text")
-                .attr("style", "border-color: #000000")
-                .attr("id", "populationValue");
-                
-    divElement.append("br");
-    divElement.append("br");
-    
-    divElement.append("input")
-                .attr("type", "button")
-                .attr("onclick", "populationMeanEntered()")
-                .attr("id", "oneSampleTestButton")
-                .attr("style", "fill: url(#buttonFillNormal); filter: url(#Bevel)")
-                .attr("value","TEST AGAINST POPULATION ESTIMATE");
-    
+   canvas.append("text")
+            .attr("x", centerX)
+            .attr("y", centerY - dialogBoxHeight/4)
+            .attr("fill", "black")
+            .attr("text-anchor", "middle")
+            .attr("font-size", fontSizeVariablePanel + "px")
+            .text("HOW TO REPORT")
+            .attr("id", "reporting")
+            .attr("class", "dialogBox");
 
 }
