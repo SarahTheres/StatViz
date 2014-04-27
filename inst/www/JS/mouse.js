fa4fb28b241f0ee7f7d133b8fdba5825bd2a2936
SarahTheres <sarah-voelkel@gmx.de>
2014-04-27 14:06:14 +0200

    Update mouse.js
    Try to make Eventhandler for reporting button work

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -1353,6 +1353,7 @@ function OnMouseDown(e)
             //if Button "SHOW REPORTING" is pressed the function reportingResults in reporting.js is called
             else if((e.button == 1 && window.event != null || e.button == 0) && target.className.baseVal == "reporting")
             {
+                setup(e, target); 
                 reportingResults();
             }
 
