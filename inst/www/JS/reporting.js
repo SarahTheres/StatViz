a4e013e4a42de8137bab3d8d24ef337457e8edd5
SarahTheres <sarah-voelkel@gmx.de>
2014-04-27 23:02:12 +0200

    Update reporting.js
    Adding Text to dialogbox

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -1,18 +1,7 @@
 function reportingResults()
 {
-   //var window = window.open("");
-   //if (window!=null) {  // Hat das Öffnen 
-                         // des Fensters geklappt?
-   //   window.document.open();  // Öffnen für 
-                                // Schreibzugriff, Pflicht!
-   //   window.document.write(tag("html") + tag("body"));
-     // window.document.write(tag("h1") + "Reporting" +
-       // tag ("/h1"));
-      //window.document.write(tag("/body") +
-        //tag ("/html"));
-      //window.document.close();  // Schreibzugriff
-                                 // beenden, Pflicht!
-var canvas = d3.select("#plotCanvas");
+   
+    var canvas = d3.select("#plotCanvas");
     
     var dialogBoxHeight = plotHeight/2;
     var dialogBoxWidth = plotWidth/2;
@@ -20,8 +9,8 @@ var canvas = d3.select("#plotCanvas");
     var centerX = canvasWidth/2;
     var centerY = canvasHeight/2;
     
-    var variableList = sort(currentVariableSelection);
-    
+    //var variableList = sort(currentVariableSelection);
+    //by declaring elements as class dialogBox they are removed when calling resetSVGCanvas() 
     canvas.append("rect")
             .attr("x", centerX - dialogBoxWidth/2)
             .attr("y", centerY - dialogBoxHeight/2)
@@ -44,5 +33,17 @@ var canvas = d3.select("#plotCanvas");
             .text("HOW TO REPORT")
             .attr("id", "reporting")
             .attr("class", "dialogBox");
+            
+   canvas.append("text")
+            .attr("x", centerX)
+            .attr("y", centerY - dialogBoxHeight/4)
+            .attr("fill", "black")
+            .attr("text-anchor", "left")
+            //TODO: allgemein deklarieren
+            .attr("font-size", "8px")
+            .text("Reporting all your results and preconditions is very important so that any other researcher can repeat your study.")
+            .attr("id", "reporting")
+            .attr("class", "dialogBox");
+      
 
 }
