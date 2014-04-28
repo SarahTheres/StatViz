f6e4c3f32bc12087f2186a78b1bbed6d3132529b
SarahTheres <sarah-voelkel@gmx.de>
2014-04-28 12:16:18 +0200

    Update reporting.js

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -87,25 +87,25 @@ function reportingResults()
             .attr("text-anchor", "left")
               //TODO: declare font size independently
             .attr("font-size", fontSizeSignificanceTestResults)
-            .text(p)
+            .text("1:" + p)
             .attr("id", "reporting")
             .attr("class", "dialogBox");
      
      canvas.append("text")
-            .attr("x", leftX + 20)
-            .attr("y", topY + 5*stepY)
+            .attr("x", leftX)
+            .attr("y", topY + 6*stepY)
             .attr("fill", "black")
             .attr("text-anchor", "left")
               //TODO: declare font size independently
             .attr("font-size", fontSizeSignificanceTestResults)
-            .text(output.p)
+            .text("2:" + output.p)
             .attr("id", "reporting")
             .attr("class", "dialogBox");
    
     //shows effect size           
    canvas.append("text")
             .attr("x", leftX)
-            .attr("y", topY + 6*stepY)
+            .attr("y", topY + 7*stepY)
             .attr("fill", "black")
             .attr("text-anchor", "left")
               //TODO: declare font size independently
