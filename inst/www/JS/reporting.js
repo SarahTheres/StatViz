1370453cc4c0dd5681b74d855d66a719be17c780
SarahTheres <sarah-voelkel@gmx.de>
2014-04-29 13:31:06 +0200

    Update reporting.js

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -95,14 +95,7 @@ function reportingUnpairedTTest()
     var topY = (centerY - dialogBoxHeight/2) + stepY;
     var leftX = centerX - dialogBoxWidth/2;
    
-   //var dialogBox = document.getElementById("reportingBox");
-   var div = document.createElement('div');
-   div.innerHTML = "my <b>new</b> skill - <large>DOM maniuplation!</large>";
-   // set style
-   div.style.color = 'red';
-   // better to use CSS though - just set class
-   //div.setAttribute('class', 'myclass'); // and make sure myclass has some styles in css
-   document.body.appendChild(div);
+  
    
    //shows current Method         
    canvas.append("text")
