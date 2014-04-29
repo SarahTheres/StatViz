15d0a111f51431c24256a08cac83c3a0abfe1519
SarahTheres <sarah-voelkel@gmx.de>
2014-04-29 21:25:50 +0200

    Update reporting.js
    inserting iv and dv to unpaired t-test

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -109,8 +109,11 @@ function reportingUnpairedTTest()
    textfield.style.top = mytop + "px";
    textfield.style.left = myleft + "px";
   
-  //TODO: insert correct dv and iv and mean, se
-   var text = "An Unpaired t-test has been conducted to compare the DV between" + testResults["independent-variable-level0"] + "IV 1 and IV 2 ";
+  //method and DV is reported
+   var text = "An Unpaired t-test has been conducted to compare the " + testResults["dependent-variable"] + " between "; 
+   
+   //IV in both groups with their means and standard errors are added
+   text += testResults["independent-variable-level0"] + " and " + testResults["independent-variable-level1"] + " groups.";
    
    //get pure p value without letter p or any operators 
    var p = getPurePValue(testResults["p"]);
