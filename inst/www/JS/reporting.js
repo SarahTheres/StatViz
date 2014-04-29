80c56e3a107956a2304087c35d88bde4aeeeeef7
SarahTheres <sarah-voelkel@gmx.de>
2014-04-29 22:15:27 +0200

    Update reporting.js

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -114,11 +114,12 @@ function reportingUnpairedTTest()
    
    //IV in both groups with their means and standard errors are added
    var variableList = getSelectedVariables();
+   var variableName = variableList["independent"][0];
+   
    console.log("variable List:" + variableList["independent"][0]);
    console.log("dataset" + dataset);
-   console.log(variables[variableList["independent"][0]][dataset]);
    
-   var mean1 = mean(variables[variableList["independent"][0]][dataset]);
+   var mean1 = mean(variables[variableName][dataset]);
    console.log("Mean 1: " + mean1);
    text += testResults["independent-variable-level0"] + "( and ";
    text += testResults["independent-variable-level1"] + " groups.";
