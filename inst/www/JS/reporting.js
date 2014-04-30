aef088ecf485468c5b633dc5ea40dd0eaec284d7
SarahTheres <sarah-voelkel@gmx.de>
2014-04-30 15:51:25 +0200

    Update reporting.js

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -98,6 +98,7 @@ function reportingUnpairedTTest()
    
     //create a new div element for reporting the text 
    //var reportingText = d3.select("body").append("div").attr("style", "position: absolute; left: " + centerX + "px; top: " + mytop + "px; height: " + dialogBoxHeight + "px; width: " + dialogBoxWidth + "px; text-align: left;").attr("class", "reporting");
+   reportingText = d3.select("body").append("div").attr("style", "position: absolute; left: " + centerX + "px; top: " + mytop + "px; height: " + dialogBoxHeight + "px; width: " + dialogBoxWidth + "px; text-align: left;").attr("class", "reporting");
 
    //method and DV is reported
    var text = "An Unpaired t-test has been conducted to compare the " + testResults["dependent-variable"] + " between "; 
