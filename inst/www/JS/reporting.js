6497684093bc5689efc2191df7ebff0638ce37f5
SarahTheres <sarah-voelkel@gmx.de>
2014-05-05 15:07:02 +0200

    Update reporting.js

--- inst/www/JS/reporting.js
+++ inst/www/JS/reporting.js
@@ -227,7 +227,7 @@ function getSignificanceTestReportingText(method)
 
 function getPostHocReportingText(method)
 {
-   var text = reportingText + "\\";
+   var text = reportingText + "\r"";
    console.log(text);
    text += getSignificanceTestReportingText(method);
    return text;
